0f93d0796a40d004381400533532eecf5b20715b
Muhammad751sheharyar <sheharyar117794@gmail.com>
2025-11-19 11:06:15 +0500

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+// import image from '../public/blog 3.jpg.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="frontimage">
+        <div class="Header">
+          <div class="logo"></div>
+          <div class="navigation">
+            <ul>
+              <li>Home</li>
+              <li><a href='#AboutUs'>About</a></li>
+              <li><a href='#Arrival'>Arrval</a></li>
+              <li>Contact</li>
+            </ul>
+          </div>
+          <div class="loginform">
+            <div class="login">LOGIN</div>
+            <div class="sinup">SINUP</div>
+          </div>
+        </div>
+      </div>
+      <div class="transparent">AboutUs</div>
+      <h1 id="AboutUs">AboutUs</h1>
+      <p class="homepara">"Welcome to Malik Brand   premium fabrics, all designed to make you feel confident and stylish. Whether you're looking for casual wear, formal attire, or something for special occasions, we have something for every mood and moment. Explore our collection
+        and experience fashion that fits your lifestyle."
+      </p>
+      <h1 class="heading" id="Arrival">Here  all  of the Fashion </h1>
+      <div class="blog">
+        <div class="blog1"><div class="blog1Image"></div>
+          <div class="blog1Text"><b>pantshart</b>Rs: 2000</div>
+        </div>
+
+        <div class="blog2">
+          <div class="blog2Image"></div>
+          <div class="blog2Text"><b>Kurta pajama</b>Rs: 2000 </div>
+        </div>
+        <div class="blog3">
+          <div class="blog3Image"></div>
+
+          <div class="blog3Text"><b>Tauzer Kit </b>Rs: 2000 </div>
+        </div>
+      </div >
+      <div class="content">
+        <div class="c1"></div>
+        <div class="c2"></div>
+      </div>
+
+
     </div>
-  );
+  )
 }
 
 export default App;
