1c9c73f88c0fe845b64ed72b3cc22d5771670dab
Muhammad751sheharyar <sheharyar117794@gmail.com>
2025-11-21 11:32:51 +0500

    components

---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,18 @@
+
+      <div class="frontimage">
+        <div class="Header">
+          <div class="logo"></div>
+          <div class="navigation">
+            <ul>
+              <li>Home</li>
+              <li><a href='#AboutUs'>About</a></li>
+              <li><a href='#Arrival'>Arrval</a></li>
+              <li>Contact</li>
+            </ul>
+          </div>
+          <div class="loginform">
+            <div class="login">LOGIN</div>
+            <div class="sinup">SINUP</div>
+          </div>
+        </div>
+      </div>
